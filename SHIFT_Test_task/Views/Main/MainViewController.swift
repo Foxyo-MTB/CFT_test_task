3abf1afe2be6f01b192792b88ff51b1123a3c401
Foxyo_MTB <98238624+Foxyo-MTB@users.noreply.github.com>
2023-01-20 20:19:10 +0300

    Deletion implemented

--- SHIFT_Test_task/Views/Main/MainViewController.swift
+++ SHIFT_Test_task/Views/Main/MainViewController.swift
@@ -17,15 +17,17 @@ class MainViewController: UIViewController, Routable, UITextFieldDelegate {
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         baseView.mainTableViewProvidesToVC().dataSource = self
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add record", style: .plain, target: self, action: #selector(addTapped))
+        baseView.mainTableViewProvidesToVC().delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Добавьте заметку", style: .plain, target: self, action: #selector(addTapped))
         loadRecords()
+        firstHardcodedRecord()
     }
-
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         baseView.frame = view.bounds
@@ -33,41 +35,49 @@ class MainViewController: UIViewController, Routable, UITextFieldDelegate {
         loadRecords()
     }
     
+    private func firstHardcodedRecord() {
+        if taskArray.isEmpty == true {
+            let hardCodedRecord = Record(context: self.context)
+            hardCodedRecord.name = "Первая заметка, которая по заданию уже должна быть отображена при первом запуске"
+            saveRecord()
+        } else {
+            print("Hardcoded record already exists")
+        }
+    }
+    
     @objc func addTapped() {
+        
         var textField = UITextField()
-        let alert = UIAlertController(title: "Добавить новую категорию", message: "", preferredStyle: .alert) //Creating Alert frame
-        let action = UIAlertAction(title: "Добавить категорию", style: .default) { (action) in //Creating button "Add category"
-            //What will happen when pressed
-            let newRecord = Record(context: self.context)                             // Initializating our new item as Categories class item =)
-            newRecord.name = textField.text                                               // Getting text of category to Categories()
-            self.taskArray.append(newRecord)                                         // Adding new category to categoriesArray.
-            self.saveRecord()                                                           // Call function for save categories.
+        let alert = UIAlertController(title: "Добавить новую заметку", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Добавить заметку", style: .default) { (action) in
+            let newRecord = Record(context: self.context)
+            newRecord.name = textField.text
+            self.taskArray.append(newRecord)
+            self.saveRecord()
         }
         action.isEnabled = false
-        alert.addTextField { (alertTextField) in                                                    // What will be printed in text field.
+        alert.addTextField { (alertTextField) in
             NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: alertTextField, queue: OperationQueue.main, using:
                                                     {_ in
                 alertTextField.delegate = self
-                // Being in this block means that something fired the UITextFieldTextDidChange notification.
-                // Access the textField object from alertController.addTextField(configurationHandler:) above and get the character count of its non whitespace characters
                 let textCount = alertTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).count ?? 0
                 let textIsNotEmpty = textCount > 0
                 // If the text contains non whitespace characters, enable the OK Button
                 action.isEnabled = textIsNotEmpty
                 
             })
-            alertTextField.placeholder = "Создайте новую категорию"                               // Gray text in text field.
-            textField = alertTextField                                                           // Store what printed in textField variably.
-            textField.autocapitalizationType = .sentences                                        // Making First letter capital.
+            alertTextField.placeholder = "Создайте новую заметку"
+            textField = alertTextField
+            textField.autocapitalizationType = .sentences
         }
-        alert.addAction(action)                                                                // Creating button "Add category".
+        alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
         alert.addAction(cancelAction)
         
     }
-
+    
 }
 
 extension MainViewController: UITableViewDataSource {
@@ -81,25 +91,25 @@ extension MainViewController: UITableViewDataSource {
         let cell = baseView.mainTableViewProvidesToVC().dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let record = taskArray[indexPath.row]
         cell.textLabel?.text = record.name
+        cell.textLabel?.numberOfLines = 0
+        cell.selectionStyle = .none
         return cell
     }
-    
 }
-
-extension MainViewController {
+// Extension for save/load/delete
+extension MainViewController: UITableViewDelegate {
     
-    func saveRecord() {
+    private func saveRecord() {
         
         do {
-            try context.save()                                      // Do-catch block for .save because this method throws an error.
+            try context.save()
         } catch {
             print("Error saving context, \(error)")
         }
-        baseView.mainTableViewProvidesToVC().reloadData()                                      // Shows data IRL on screen
+        baseView.mainTableViewProvidesToVC().reloadData()
     }
     
-    func loadRecords(with request: NSFetchRequest<Record> = Record.fetchRequest()) {            // With - external parameter, request - internal parameter. = Categories.fetchRequest() is a default value here.
-        //let request : NSFetchRequest<Categories> = Categories.fetchRequest()    // Specifying type of request, because a lot of different data fetches. <Return an array of items> - meaning. Commented after refactoring code and adding external/internal parameter on string above.
+    private func loadRecords(with request: NSFetchRequest<Record> = Record.fetchRequest()) {            // With - external parameter, request - internal parameter. = Record.fetchRequest() is a default value here.
         do {
             taskArray = try context.fetch(request)                  // Do-catch block for .fetch because this method throws an error.
         } catch {
@@ -108,4 +118,19 @@ extension MainViewController {
         baseView.mainTableViewProvidesToVC().reloadData()
     }
     
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        print("lalal")
+        let contextItem = UIContextualAction(style: .destructive, title: "Удалить") {  (contextualAction, view, boolValue) in
+            self.context.delete(self.taskArray[indexPath.row])                                                // Delete from Core Data.
+            self.taskArray.remove(at: indexPath.row)                                                          // Delete from Array.
+            do {
+                try self.context.save()                                                                             //Renew of our Core Data.
+                self.baseView.mainTableViewProvidesToVC().reloadData()
+            } catch {
+                print(error)
+            }
+        }
+        let swipeActions = UISwipeActionsConfiguration(actions: [contextItem])
+        return swipeActions
+    }
 }
